bff1328bdd617777c9c3434c0b35c12efa5cfce0
AFurrow <austindfurrow@gmail.com>
2020-09-17 20:41:24 -0600

    Finished fibonacci functions

---
+++ fibonacci.rb
@@ -0,0 +1,22 @@
+def fibs(num)
+  fibs_arr = []
+  num.times do |x|
+    if x < 2
+      fibs_arr.push(x) 
+    else
+      fib = fibs_arr[-1] + fibs_arr[-2]
+      fibs_arr.push(fib) 
+    end
+  end
+  fibs_arr
+end
+
+def fibs_rec(num)
+  return [0, 1].slice(0, num) if num < 3 
+  arr = fibs_rec(num - 1)
+  arr << (arr[-1] + arr[-2])
+end
+
+sequences = 25
+p fibs(sequences)
+p fibs_rec(sequences)
